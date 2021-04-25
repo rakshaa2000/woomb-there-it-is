3785520b9446eb1a9b4214a0cfa67250855128b0
Rakshaa Viswanathan <46165429+rakshaa2000@users.noreply.github.com>
2021-04-25 19:47:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# woomb-there-it-is
+ 
---
+++ script.js
@@ -0,0 +1,61 @@
+window.onload = init;
+function init() {
+    var levelno = 1;
+    var level = document.getElementById('levelhead');
+    var img = document.getElementById('level');
+	if (window.Event) {
+	document.captureEvents(Event.MOUSEMOVE);
+	}
+	document.onclick = getCursorXY;
+    img.src='./level'+levelno+'.png';
+}
+
+var cuterus = {
+    xstart : 410, 
+    xend : 530, 
+    ystart : 320, 
+    yend : 450
+}
+var lovelace = {
+    xstart : 500, 
+    xend : 620, 
+    ystart : 100, 
+    yend : 270
+}
+var kamala = {
+    xstart : 1150, 
+    xend : 1400, 
+    ystart : 330, 
+    yend : 420
+}
+var rbg = {
+    xstart : 1100, 
+    xend : 1170, 
+    ystart : 100, 
+    yend : 200
+}
+var marie = {
+    xstart : 1170, 
+    xend : 1300, 
+    ystart : 100, 
+    yend : 300
+}
+function getCursorXY(e) {
+	var X = e.pageX;
+	var Y = e.pageY;
+    if(X>=cuterus.xstart && X<=cuterus.xend && Y>=cuterus.ystart && Y<=cuterus.yend){
+        alert('Found the Cuterus');
+    }
+    if(X>=lovelace.xstart && X<=lovelace.xend && Y>=lovelace.ystart && Y<=lovelace.yend){
+        alert('Found Ada Lovelace');
+    }
+    if(X>=kamala.xstart && X<=kamala.xend && Y>=kamala.ystart && Y<=kamala.yend){
+        alert('Found Kamala Harris');
+    }
+    if(X>=rbg.xstart && X<=rbg.xend && Y>=rbg.ystart && Y<=rbg.yend){
+        alert('Found Ruth Bader Ginsberg');
+    }
+    if(X>=marie.xstart && X<=marie.xend && Y>=marie.ystart && Y<=marie.yend){
+        alert('Found Marie Curie');
+    }
+}
